b99bb18446893359b6bb2dbfe311c319371d08e5
Scott Robertson <sc0tt5@users.noreply.github.com>
2019-02-12 20:00:49 -0600

    store selectors and async pipe

--- src/products/containers/product-item/product-item.component.ts
+++ src/products/containers/product-item/product-item.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import * as fromStore from '../../store';
 
 import { Pizza } from '../../models/pizza.model';
@@ -23,7 +24,7 @@ import { Topping } from '../../models/topping.model';
         (update)="onUpdate($event)"
         (remove)="onRemove($event)"
       >
-        <pizza-display [pizza]="visualise"> </pizza-display>
+        <pizza-display [pizza]="visualise$ | async"> </pizza-display>
       </pizza-form>
     </div>
   `
@@ -31,20 +32,29 @@ import { Topping } from '../../models/topping.model';
 export class ProductItemComponent implements OnInit {
   // pizza: Pizza; // changing pizza over to an observable
   pizza$: Observable<Pizza>; // pizza to pizza$ (also set to Observable) and update in template as well
-  visualise: Pizza;
+  visualise$: Observable<Pizza>;
   toppings$: Observable<Topping[]>;
 
   constructor(private store: Store<fromStore.ProductsState>) {}
 
   ngOnInit() {
-    this.store.dispatch(new fromStore.LoadToppings());
     // because we're now selecting data from the store we will add the $ suffix
     // assign the selector
-    this.pizza$ = this.store.select(fromStore.getSelectedPizza); // we're using a query via our selector
+    this.pizza$ = this.store.select(fromStore.getSelectedPizza).pipe(
+      // tap is used because do is a js reserved word
+      tap((pizza: Pizza = null) => {
+        const pizzaExists = !!(pizza && pizza.toppings);
+        const toppings = pizzaExists ? pizza.toppings.map(topping => topping.id) : []; // clear state if not exist
+        this.store.dispatch(new fromStore.VisualiseToppings(toppings));
+      })
+    ); // we're using a query via our selector
     this.toppings$ = this.store.select(fromStore.getAllToppings);
+    this.visualise$ = this.store.select(fromStore.getPizzaVisualised);
   }
 
-  onSelect(event: number[]) {}
+  onSelect(event: number[]) {
+    this.store.dispatch(new fromStore.VisualiseToppings(event));
+  }
 
   onCreate(event: Pizza) {}
 
